b58871c7c0148e9ac2a9cb959c7d318f99e11eba
MJMoshiri <thisismjmoshiri@gmail.com>
2023-06-04 21:25:30 +0300

    change kotlin version to 1.8.21

--- app/src/main/java/com/android/secondsight/data/Interval.kt
+++ app/src/main/java/com/android/secondsight/data/Interval.kt
@@ -1,9 +1,11 @@
 package com.android.secondsight.data
 
 import kotlin.time.Duration
+import kotlin.time.ExperimentalTime
 import kotlin.time.TimeSource
 
 // Represents a specific period of time spent on a task.
+@OptIn(ExperimentalTime::class)
 data class Interval(
     val start: TimeSource.Monotonic.ValueTimeMark,
     val end: TimeSource.Monotonic.ValueTimeMark,
--- app/src/main/java/com/android/secondsight/data/TaskEntry.kt
+++ app/src/main/java/com/android/secondsight/data/TaskEntry.kt
@@ -1,9 +1,11 @@
 package com.android.secondsight.data
 
 import kotlin.time.Duration
+import kotlin.time.ExperimentalTime
 import kotlin.time.TimeSource
 
 // Represents a specific instance of time tracking for a task. This might include multiple intervals if you pause and resume tracking.
+@OptIn(ExperimentalTime::class)
 data class TaskEntry(
     val id: String,
     val taskId: String,
