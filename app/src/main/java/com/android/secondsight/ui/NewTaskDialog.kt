6ec157895e764d0afb0abdb5dd15514c3718591d
MJMoshiri <thisismjmoshiri@gmail.com>
2023-06-08 00:37:12 +0300

    edit TaskList page layout

---
+++ app/src/main/java/com/android/secondsight/ui/NewTaskDialog.kt
@@ -0,0 +1,67 @@
+@file:OptIn(ExperimentalMaterial3Api::class)
+
+package com.android.secondsight.ui
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.onFocusChanged
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun NewTaskDialog(onConfirm: (String, String) -> Unit, onCancel: () -> Unit) {
+    var name by remember { mutableStateOf("") }
+    var description by remember { mutableStateOf("") }
+    var isError by remember { mutableStateOf(false) }
+
+    AlertDialog(onDismissRequest = onCancel, title = { Text(text = "New Task") }, text = {
+        Column {
+            OutlinedTextField(modifier = Modifier
+                .fillMaxWidth()
+                .onFocusChanged { focusState ->
+                    if (focusState.isFocused) {
+                        isError = false
+                    }
+                },
+                shape = RoundedCornerShape(8.dp),
+                value = name,
+                onValueChange = { name = it },
+                isError = isError,
+                singleLine = true,
+                label = { Text(text = "Name") })
+            Spacer(modifier = Modifier.height(8.dp))
+            OutlinedTextField(value = description,
+                modifier = Modifier.fillMaxWidth(),
+                shape = RoundedCornerShape(8.dp),
+                onValueChange = { description = it },
+                label = { Text(text = "Description") })
+        }
+    }, confirmButton = {
+        Button(onClick = {
+            if (name.isNotBlank()) {
+                onConfirm(name, description)
+            } else {
+                isError = true
+            }
+        }) {
+            Text(text = "Add")
+        }
+    }, dismissButton = {
+        Button(onClick = onCancel) {
+            Text(text = "Cancel")
+        }
+    })
+}
--- app/src/main/java/com/android/secondsight/ui/TaskList.kt
+++ app/src/main/java/com/android/secondsight/ui/TaskList.kt
@@ -7,32 +7,24 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
-import androidx.compose.material3.AlertDialog
-import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
-import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.focus.onFocusChanged
 import androidx.compose.ui.unit.dp
 import com.android.secondsight.data.Task
 import com.android.secondsight.viewmodel.TaskListViewModel
@@ -90,48 +82,5 @@ fun TaskList(tasks: List<Task>) {
     }
 }
 
-@Composable
-fun NewTaskDialog(onConfirm: (String, String) -> Unit, onCancel: () -> Unit) {
-    var name by remember { mutableStateOf("") }
-    var description by remember { mutableStateOf("") }
-    var isError by remember { mutableStateOf(false) }
 
-    AlertDialog(onDismissRequest = onCancel, title = { Text(text = "New Task") }, text = {
-        Column {
-            OutlinedTextField(modifier = Modifier
-                .fillMaxWidth()
-                .onFocusChanged { focusState ->
-                    if (focusState.isFocused) {
-                        isError = false
-                    }
-                },
-                shape = RoundedCornerShape(8.dp),
-                value = name,
-                onValueChange = { name = it },
-                isError = isError,
-                singleLine = true,
-                label = { Text(text = "Name") })
-            Spacer(modifier = Modifier.height(8.dp))
-            OutlinedTextField(value = description,
-                modifier = Modifier.fillMaxWidth(),
-                shape = RoundedCornerShape(8.dp),
-                onValueChange = { description = it },
-                label = { Text(text = "Description") })
-        }
-    }, confirmButton = {
-        Button(onClick = {
-            if (name.isNotBlank()) {
-                onConfirm(name, description)
-            } else {
-                isError = true
-            }
-        }) {
-            Text(text = "Add")
-        }
-    }, dismissButton = {
-        Button(onClick = onCancel) {
-            Text(text = "Cancel")
-        }
-    })
-}
 
