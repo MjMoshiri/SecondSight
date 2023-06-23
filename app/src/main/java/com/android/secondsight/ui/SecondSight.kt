7833f1aafad7b565dbbcfee3ba30a03f149534ac
MJMoshiri <thisismjmoshiri@gmail.com>
2023-06-23 22:08:20 +0300

    add drawer, move theme switch to it, add new theme switch component

--- app/src/main/java/com/android/secondsight/ui/SecondSight.kt
+++ app/src/main/java/com/android/secondsight/ui/SecondSight.kt
@@ -1,21 +1,38 @@
 package com.android.secondsight.ui
 
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Menu
+import androidx.compose.material3.Divider
+import androidx.compose.material3.DrawerValue
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.ModalDrawerSheet
+import androidx.compose.material3.ModalNavigationDrawer
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Switch
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.rememberDrawerState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
 import com.android.secondsight.Screen
+import com.android.secondsight.ui.util.ThemeSwitcher
 import com.android.secondsight.viewmodel.TaskListViewModel
 import com.android.secondsight.viewmodel.provider.vmProvider
+import kotlinx.coroutines.launch
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -27,51 +44,79 @@ fun SecondSight(
 ) {
     val backStackEntry by navController.currentBackStackEntryAsState()
     val currentRoute = backStackEntry?.destination?.route
+    val drawerState = rememberDrawerState(DrawerValue.Closed)
+    val scope = rememberCoroutineScope()
 
-    Scaffold(
-        topBar = {
-            TopAppBar(title = {
-                Text(
-                    text = when (currentRoute) {
-                        Screen.TaskList.route -> "Tasks"
-                        Screen.TaskDetail.route -> "Entries"
-                        Screen.Entry.route -> "Entry"
-                        else -> "Second Sight"
-                    }
-                )
-            }, actions = {
-                Switch(checked = darkTheme.value, onCheckedChange = { darkTheme.value = it })
-            })
+    ModalNavigationDrawer(drawerState = drawerState,
+        gesturesEnabled = drawerState.isOpen,
+        drawerContent = {
+            ModalDrawerSheet {
+                Text("Theme", modifier = Modifier.padding(16.dp))
+                Divider()
+                Row(
+                    modifier = Modifier
+                        .padding(16.dp)
+                        .fillMaxWidth()
+                ) {
+                    Spacer(modifier = Modifier.weight(1f))
+                    ThemeSwitcher(size = 50.dp, darkTheme = darkTheme.value, onClick = {
+                        darkTheme.value = !darkTheme.value
+                    })
+                }
+            }
         },
-    ) { innerPadding ->
-        NavHost(navController = navController, startDestination = "task_list") {
-            composable("task_list") {
-                TaskListScreen(viewModel, pd = innerPadding, onTaskClick = { taskId ->
-                    navController.navigate("task_detail/$taskId")
+        content = {
+            Scaffold(topBar = {
+                TopAppBar(title = {
+                    Text(
+                        text = when (currentRoute) {
+                            Screen.TaskList.route -> "Tasks"
+                            Screen.TaskDetail.route -> "Entries"
+                            Screen.Entry.route -> "Entry"
+                            else -> "Second Sight"
+                        }
+                    )
+                }, actions = {
+                    IconButton(onClick = {
+                        scope.launch {
+                            if (drawerState.isOpen) drawerState.close() else drawerState.open()
+                        }
+                    }) {
+                        Icon(Icons.Filled.Menu, contentDescription = "Menu")
+                    }
                 })
+            }) { innerPadding ->
+                NavHost(navController = navController, startDestination = "task_list") {
+                    composable("task_list") {
+                        TaskListScreen(viewModel, pd = innerPadding, onTaskClick = { taskId ->
+                            navController.navigate("task_detail/$taskId")
+                        })
+                    }
+                    composable("task_detail/{taskId}") {
+                        val taskId = it.arguments?.getString("taskId")!!.toLong()
+                        EntryListScreen(viewModel = vmProvider.getTaskViewModel(taskId),
+                            pd = innerPadding,
+                            createEntry = { entryId ->
+                                navController.navigate("task_detail/$taskId/entry_detail/$entryId")
+                            },
+                            selectEntry = { entryId ->
+                                navController.navigate("task_detail/$taskId/entry_detail/$entryId")
+                            })
+                    }
+                    composable("task_detail/{taskId}/entry_detail/{entryId}") {
+                        val entryId = it.arguments?.getString("entryId")!!.toLong()
+                        EntryScreen(viewModel = vmProvider.getEntryViewModel(entryId),
+                            pd = innerPadding,
+                            stopEntry = { taskId ->
+                                navController.popBackStack("task_detail/$taskId", false)
+                            })
+                    }
+                }
             }
-            composable("task_detail/{taskId}") {
-                val taskId = it.arguments?.getString("taskId")!!.toLong()
-                EntryListScreen(viewModel = vmProvider.getTaskViewModel(taskId),
-                    pd = innerPadding,
-                    createEntry = { entryId ->
-                        navController.navigate("task_detail/$taskId/entry_detail/$entryId")
-                    },
-                    selectEntry = { entryId ->
-                        navController.navigate("task_detail/$taskId/entry_detail/$entryId")
-                    })
-            }
-            composable("task_detail/{taskId}/entry_detail/{entryId}") {
-                val entryId = it.arguments?.getString("entryId")!!.toLong()
-                EntryScreen(viewModel = vmProvider.getEntryViewModel(entryId),
-                    pd = innerPadding,
-                    stopEntry = { taskId ->
-                        navController.popBackStack("task_detail/$taskId", false)
-                    })
-            }
-        }
-    }
+        })
 }
+
+
 
 
 
